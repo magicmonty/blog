d3c50ae159f5929060045f9f125851921d4b0833
Martin Gondermann <magicmonty@pagansoft.de>
2012-09-30 20:27:16 +0200

    * made github repo widget static * added tag cloud * moved widgets to widgets.jade

--- contents/js/init.js
+++ contents/js/init.js
@@ -1,22 +1,4 @@
-$(function() {
-	$.getJSON('https://api.github.com/users/magicmonty/repos?callback=?', function(resp) {
-		if (resp.data.length > 0) {
-			$('#github-repos').append('<ul></ul>');
-			$.each($(resp.data).sort('pushed_at', 'desc'), function(i, val) {
-				var description = ((val['description']) ? val['description'] : '(No description.)');
-				var url = val['html_url'];
-				var name = val['name'];
-				var selector = "github_" + name;
-
-				$('#github-repos > ul').append(
-					'<li><a href="'+url+'" title="'+name+'" rel="popover" data-placement="top" data-trigger="hover" data-content="'+description+'" class="github_link" id="github_link_' + name + '" >'+name+'</a></li>');
-			});			
-		} else {
-			$('#github-repos').append('<p>No public repositories.</p>');
-		}
-		
-		$('a[rel="popover"]').popover();
-	});	
-	
+$(function() {	
+	$('*[rel="popover"]').popover();
 	$('*[rel="tooltip"]').tooltip();
 });
