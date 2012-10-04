b97d84a2a504291fb8706ffead084c63b68b0c5f
Martin Gondermann <magicmonty@pagansoft.de>
2012-10-04 10:14:06 +0200

    added error document

--- contents/js/init.js
+++ contents/js/init.js
@@ -1,4 +1,5 @@
 $(function() {	
 	$('*[rel="popover"]').popover();
 	$('*[rel="tooltip"]').tooltip();
+	$("#error").html('The requested url "' + window.location + '" was not found on this server!')
 });
