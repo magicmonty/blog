44fd140a7bbfa78d6f4d9a6f44fe34734c741184
Martin Gondermann <magicmonty@pagansoft.de>
2013-02-17 14:10:28 +0100

    Made build script compatible with Mac OS X

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 compass compile && wintersmith build
-find build -type f -print0 | xargs -0 md5sum > md5sums.txt
+
+MD5=md5sums
+
+if [ -x /sbin/md5 ]; then
+	MD5="md5 -r"
+fi 
+
+find build -type f -print0 | xargs -0 $MD5 > md5sums.txt
