cdfd6d3d96a83d21bf3aa617bef6dccbb670f280
Martin Gondermann <magicmonty@pagansoft.de>
2013-08-04 19:59:43 +0200

    Fixed deploy script

--- deploy.rb
+++ deploy.rb
@@ -44,7 +44,9 @@ if !system("./node_modules/wintersmith/bin/wintersmith build")
 end
 
 if !system("find build -type f -print0 | xargs -0 md5 -r > md5sums.txt")
-    exit 1
+    if !system("find build -type f -print0 | xargs -0 md5sum -r > md5sums.txt")
+        exit 1
+    end
 end
 
 origFileName = "md5sums.orig"
