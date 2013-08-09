226a8aeea2be9e8b25d35737cb0967d5a33872d1
Martin Gondermann <magicmonty@pagansoft.de>
2013-08-09 20:22:06 +0200

    updated build scripts to use rake

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @ECHO OFF
-ECHO Compiling CSS ...
-cmd /C compass compile
-IF ERRORLEVEL 1 GOTO compass_error
 
-ECHO Building blog ...
-node_modules\.bin\wintersmith build
-IF ERRORLEVEL 1 GOTO build_error
-GOTO quit
-
-:compass_error
-ECHO Error compiling css files
-PAUSE
-GOTO quit
-
-:build_error
-ECHO Error building blog
-PAUSE
-GOTO quit
-
-:quit
+rake build
--- build.sh
+++ build.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-compass compile && ./node_modules/wintersmith/bin/wintersmith build
-
-MD5=md5sum
-
-if [ -x /sbin/md5 ]; then
-	MD5="md5 -r"
-fi 
-
-find build -type f -print0 | xargs -0 $MD5 > md5sums.txt
+rake build
