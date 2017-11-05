09f130198379056612d900a3edb3a41ffec34a2b
Martin Gondermann <magicmonty@pagansoft.de>
2017-11-05 21:51:28 +0100

    Fixing AMP

--- _plugins/amp_generate.rb
+++ _plugins/amp_generate.rb
@@ -9,8 +9,8 @@ module Jekyll
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'amp.html')
       self.data['body']          = post.content
-      self.data['hasIframe']     = post.content.include? "{% youtube "
-      self.data['hasTwitter']     = post.content.include? "{% twitter "
+      self.data['hasYoutube']    = post.content.include? "{% youtube "
+      self.data['hasTwitter']    = post.content.include? "{% twitter "
       self.data['title']         = post.data['title']
       self.data['date']          = post.data['date']
       self.data['author']        = post.data['author']
@@ -25,10 +25,8 @@ module Jekyll
     def generate(site)
       dir = site.config['ampdir'] || 'amp'
       site.posts.docs.each do |post|
-        index = AmpPost.new(site, site.source, File.join(dir, post.id), post)
-        index.render(site.layouts, site.site_payload)        
-        index.write(site.dest)
-        site.pages << index
+        next if post.data['skip_amp'] == true
+        site.pages << AmpPost.new(site, site.source, File.join(dir, post.id), post)
       end
     end
   end
