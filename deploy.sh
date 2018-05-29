c9c4fe08add1029520db6cc204895e070cc8af2f
Martin Gondermann <magicmonty@pagansoft.de>
2018-05-29 22:57:20 +0200

    Add deploy and vendor folders to ignore

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -rf deploy >/dev/null
+git clone git@github.com:magicmonty/blog.git --branch gh-pages --single-branch --depth 1 deploy
+cd deploy
+git rm -rf .
+cd ..
+bundle exec jekyll build --destination deploy || exit 1
+cd deploy
+git add --all
+git commit -m "Deployment"
+git push origin gh-pages
+cd ..
+rm -rf deploy
+git fetch -p
